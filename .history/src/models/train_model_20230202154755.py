f735b72b0864b9e8e5a11a0e6332d584fc3d241f
H04K <th.hesters@gmail.com>
2023-02-02 15:48:09 +0100

    train_model_update

--- .history/src/models/train_model_20230202154755.py
+++ .history/src/models/train_model_20230202154755.py
@@ -11,7 +11,7 @@ wandb.login()
 import pandas as pd 
 
 #get the data
-df = pd.read_csv('../data/Full_dataset.csv')
+df = pd.read_csv('../Full_dataset.csv')
 df.insert(0, "prefix", "QA")
 #Last minute change to the data
 df["target_text"] = df["target_text"].str.replace(r'<a href="(.+?)">(.+?)</a>', r'[\2](\1)')
